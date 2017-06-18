cfe402376b5ce46e356ef07f2e41670b104ab1e1
TAMA <akbar@javan.co.id>
2017-06-19 04:28:39 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/auth", auth)
+	http.HandleFunc("/", sayhelloName)       // set router
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(1000) // parse arguments, you have to call this by yourself
+	// fmt.Println(r.Form) // print form information in server side
+	// fmt.Println("path", r.URL.Path)
+	// fmt.Println("scheme", r.URL.Scheme)
+	// fmt.Println(r.Form["url_long"])
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+
+	fmt.Println(r.Form)
+	fmt.Println(r.MultipartForm)
+
+	// for k, v := range r.Form {
+	// 	fmt.Println("key:", k)
+	// 	fmt.Println("val:", strings.Join(v, ""))
+	// }
+	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
+}
